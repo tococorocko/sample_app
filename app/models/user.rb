4566a97f134a070d16215b980e84adafd2e09d27
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-12 18:57:38 +0100

    add callback to save email downcased

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Callback to downcase email befor saving it to the db
+  before_save { self.email = email.downcase }
   # See test/models/user_test.rb
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
