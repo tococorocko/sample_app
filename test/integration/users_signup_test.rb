5023ed65077cb7aa32a457571764085fbf941a28
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-13 17:01:31 +0100

    add signup test for succesfull signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,6 +14,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'form[action="/signup"]'
     # Test
-    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name:  "Valid Name",
+                                         email: "user@example.com",
+                                         password:              "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+    assert_not flash.empty?
+  end
+
 end
