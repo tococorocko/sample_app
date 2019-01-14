6a2611c8b0bb474c08dac339481c8465825f4ec0
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-14 16:57:59 +0100

    add test for users_login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      # Create an error message. No Model for sessions, so no active record error messages. .now necessary so error message disapears after render
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
