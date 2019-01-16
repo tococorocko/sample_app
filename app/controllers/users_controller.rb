ca85f2a832608342cf54cfeb6a9ff3a5c2206f30
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-16 17:05:27 +0100

    add authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
-    @user = User.find(params[:id])
+    # see before_action
   end
 
   def new
@@ -19,11 +24,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # see before_action
   end
 
   def update
-    @user = User.find(params[:id])
+    # before_action
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -37,5 +42,26 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+
+  #beofre filters:
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
