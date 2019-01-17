ba01120cf61dfa6bbd2f42ef797faeeae15fc444
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-17 14:32:47 +0100

    add micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  # Callback to downcase email befor saving it to the db
+  has_many :microposts
+
+  # Callback to downcase email befor saving it to the db
   before_save { self.email = email.downcase }
   # See test/models/user_test.rb
   validates :name, presence: true, length: { maximum: 50 }
